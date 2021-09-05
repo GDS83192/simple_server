11a1975ab00fe35ab4ee021d8da8cef44b72f450
graha <graham.squires3@gmail.com>
2021-09-05 12:44:27 -0400

    close

--- server/server.js
+++ server/server.js
@@ -1,30 +1,17 @@
-import express from 'express'
-import devBundle from './devBundle'
-import path from 'path'
-import template from '../template'
-import { MongoClient } from 'mongodb'
+import config from './../config/config'
+import app from './express'
+import mongoose from 'mongoose'
 
-const CURRENT_WORKING_DIR = process.cwd()
-const app = express()
-
-app.use('./dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
-devBundle.compile(app)
-
-
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
-MongoClient.connect(url, (err, db) => {
-    console.log("Connected successfully to mongodb server")
-    db.close()
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
-app.get('/', (req, res) => {
-    res.status(200).send(template())
-})
-
-let port = process.env.PORT || 3000
-app.listen(port, function onStart(err) {
+app.listen(config.port, (err) => {
     if (err) {
         console.log(err)
     }
-    console.info('Server started on port %s.', port)
+    console.info('Server started on port %s.', config.port)
 })
